31283b810266fce53179453cfd5be247e490d90b
Oliver <Test@googlemail.com>
2017-02-28 17:05:05 +0000

    Changes

--- web/index.php
+++ web/index.php
@@ -1,9 +1,11 @@
 <?php
 
-require('../vendor/autoload.php');
+require __DIR__. '../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
+$app['controllers']
+    ->requireHttps(); //We can change it so only some pages require https
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -14,6 +16,11 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
+
+// Create our routing controllers
+$protected = $app['controllers_factory'];
+$protected->before();
+
 
 // Our web handlers
 
