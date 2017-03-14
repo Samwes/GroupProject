e25f47b58edec9cd9e85cdc6305995e252608d38
Oliver <Test@googlemail.com>
2017-03-14 07:42:37 +0000

    Stuff

--- filefinder.sh
+++ filefinder.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 obj_name="$1"
 shift
+NFOUND=true
 git log "$@" --pretty=format:'%T %h %s' \
 | while read tree commit subject ; do
     if git ls-tree -r $tree | grep -q "$obj_name" ; then
         echo $commit "$subject"
-    fi
+		NFOUND=false
+	fi
 done
+if $NFOUND; then
+	echo "Not Found"
+fi
---
+++ filesizer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# git rev-list --objects master |git cat-file --batch-check='%(objectsize) %(objecttype) %(objectname) %(rest)'
+
+git rev-list HEAD |                     # list commits
+  xargs -n1 git ls-tree -rl |             # expand their trees
+  sed -e 's/[^ ]* [^ ]* \(.*\)\t.*/\1/' | # keep only sha-1 and size
+  sort -u |                               # eliminate duplicates
+  awk '{ sum += $2 } END { print sum }'   # add up the sizes in bytes
