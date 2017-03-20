9f303856bcffd81dca55d95f3c637f1eb2655739
Sam Bond-Taylor <samb-t@hotmail.co.uk>
2017-03-20 13:22:59 +0000

    Upload Form Work

--- web/js/scanner.js
+++ web/js/scanner.js
@@ -243,3 +243,13 @@ $(function() {
         });
     });
 });
+
+// Taken From Stack Overflow
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+// Taken From Stack Overflow
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
