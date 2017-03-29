2b5b44e19f745d699b4b0c88690fa6b2b7d41cc8
Sam Bond-Taylor <samb-t@hotmail.co.uk>
2017-03-29 18:45:23 +0100

    Search Progress

--- src/Database/DBDataMapper.php
+++ src/Database/DBDataMapper.php
@@ -428,7 +428,7 @@ class DBDataMapper
       $weightQuery = "`weight` <= :maxWeight AND `weight` >= :minWeight";
 
       $query = "SELECT `foodid` FROM `itemtable` WHERE `name` LIKE '%' || :search || '%'";
-      $params = array(':search' => '%'.$search.'%');
+      $params = array(':search' => $search);
 
       if ($category != "") {
           $params[':category'] = $category;
