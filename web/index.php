52a3ca3a72168632ac46eadd6389ca0401656775
Oliver <kek>
2017-04-06 17:54:47 +0100

    Work

--- web/index.php
+++ web/index.php
@@ -1,15 +1,15 @@
 <?php
 
-define('DEBUG',true);
-define('ROOT',__DIR__);
+define('DEBUG', true);
+define('ROOT', __DIR__);
 
-require ROOT . '/../vendor/autoload.php';
+require ROOT.'/../vendor/autoload.php';
 
-if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'){
-    $_SERVER['HTTPS']='on';
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
 }
 
-$app = new Main\App(array ( 'debug' => DEBUG));
+$app = new Main\App(array('debug' => DEBUG));
 
 // -------- SERVICES --------
 //note moved to class (app.php)
@@ -28,12 +28,11 @@ $app = new Main\App(array ( 'debug' => DEBUG));
 
 //note these are debug pages to test security, remove asap or change into something usabel
 
-$app->get('/admin', function() use($app) {
-    return $app['twig']->render('admin.twig');
+$app->get('/admin', function () use ($app) {
+	return $app['twig']->render('admin.twig');
 });
 
 // ----------------------------
-
 
 // -------- ERROR HANDLING --------
 //note moved to class
