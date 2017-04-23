1f5a8f23f9e4ede4cc7d78a015b34314e99e5f34
TomRundle <ThomasRundle97@gmail.com>
2017-04-23 15:10:31 +0100

    Test
    TestLikelihood

--- src/Handlers/Requests.php
+++ src/Handlers/Requests.php
@@ -509,7 +509,7 @@ class Requests
 
 	public function foodLikelihood(Request $request, App $app, $foodid) {
 		$foodItem = $this->db->getFoodItemByID($foodid);
-		$desirableFoods = ['chocolate', 'steak', 'beef', 'lamb', 'chicken', 'burger', 'cereal', 'pizza', 'pasta' ];
+		$desirableFoods = ['chocolate', 'steak', 'beef', 'lamb', 'chicken', 'burger', 'cereal', 'pizza', 'pasta'];
 		$foodName = $foodItem['name'];
 		$probability = 60;
 
@@ -517,7 +517,7 @@ class Requests
 		for($i = 0; i<$desirableFoods.length; $i++) {
 			if (strpos( strtolower($foodName), $desirableFoods[i]) !== false) {
 							$probability =  rand ( 60 , 80 );
-							break;
+							
 
 					}
 		}
